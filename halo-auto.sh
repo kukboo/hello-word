cce5b4ed591002a214eb9f1e22b919c3a665ea24
kukboo <kukboo@yeah.net>
2020-07-04 22:33:36 +0800

    Update halo-auto.sh

--- halo-auto.sh
+++ halo-auto.sh
@@ -2,12 +2,13 @@
 # author:yituge.cn
 echo 'CentOS部署halo博客系统一键脚本'
 echo '更新yum'
-yum update -y
+sudo yum update -y
 echo '安装最新版无头openjdk'
-yum install java-latest-openjdk-headless -y
+sudo yum install java-latest-openjdk-headless -y
 echo '添加halo用户到/home'
-cd /home
-useradd -m halo
+sudo cd /home
+sudo useradd -m halo
+su halo <<!
 echo '安装halo-jar文件并重命名'
 if [ -e halo/halo-latest.jar ];then
 echo 'halo-jar文件已存在'
@@ -18,16 +19,18 @@ wget https://halo.cary.tech/release/halo-1.3.2.jar -O halo/halo-latest.jar
 fi
 echo '下载配置文件到 ~/.halo 目录'
 curl -o ~/.halo/application.yaml --create-dirs https://dl.halo.run/config/application-template.yaml
+exit
+!
 echo '下载 Halo 官方的 halo.service 模板'
-curl -o /etc/systemd/system/halo.service --create-dirs https://dl.halo.run/config/halo.service
+sudo curl -o /etc/systemd/system/halo.service --create-dirs https://dl.halo.run/config/halo.service
 echo '修改 /etc/systemd/system/halo.service'
-sed -i "s/JAR_PATH/\/home\/halo\/halo-latest.jar/g" /etc/systemd/system/halo.service
+sudo sed -i "s/JAR_PATH/\/home\/halo\/halo-latest.jar/g" /etc/systemd/system/halo.service
 echo '刷新 Systemd'
-systemctl daemon-reload
+sudo systemctl daemon-reload
 echo '使 Halo 开机自启'
-systemctl enable halo
+sudo systemctl enable halo
 echo '启动 Halo'
-service halo start
+su halo -c "service halo start"
 echo '查看 Halo 状态'
-service halo status
+su halo -c service halo status
 echo '----END----'
